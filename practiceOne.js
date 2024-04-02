14f4081fc24135a0a6c357c8160e75606d40548a
Ifeoluwa-Ajiboye <162115703+Ifeoluwa-Ajiboye@users.noreply.github.com>
2024-04-02 19:14:21 +0100

    Update practiceOne.js

--- practiceOne.js
+++ practiceOne.js
@@ -6,4 +6,26 @@ do {
   console.log(yourName);
 }
 while(!yourName);
-  
+
+------------------------------------------------------------------------------------------
+//square
+let number = 0;
+let result = 1;
+
+while(number < 10){
+  result *= 2;
+  number++;
+}
+
+-----------------------------------------------------------------------------------------
+//only if the input is actually a number
+let yourNumber = Number(prompt("Enter number"));
+
+if (!Number.isNaN(yourNumber)){
+  console.log(`Welcome, your number is ${yourNumber}`);
+}
+else{
+  console.log("error");
+}
+
+------------------------------------------------------------------------------------------
