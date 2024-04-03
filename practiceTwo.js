c0c01e50c52097ea801bb2aaef2becf431733e5d
Ifeoluwa-Ajiboye <162115703+Ifeoluwa-Ajiboye@users.noreply.github.com>
2024-04-04 00:41:32 +0100

    Update practiceTwo.js

--- practiceTwo.js
+++ practiceTwo.js
@@ -1,1 +1,28 @@
+// using rest parameter to sum numbers up
+function mySum(...args){
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
 
+---------------------------------------------------------------------
+// finding the max number using stack
+  function myMax(){
+    let max = -Infinity;
+    for (let i = 0; i < arguments.length;  i++){
+      if (arguments[i] > max){
+        max = arguments[i];
+      }
+    }
+    return max;
+  }
+
+---------------------------------------------------------------------
+  // sum  using function
+  function sumUp(){
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++){
+      sum += arguments[i];
+    }
+    return sum;
+  }
