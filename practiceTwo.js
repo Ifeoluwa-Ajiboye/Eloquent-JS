08ca3a0568dca13fc4676bfd0beba5efc69c8e13
Ifeoluwa-Ajiboye <162115703+Ifeoluwa-Ajiboye@users.noreply.github.com>
2024-04-04 14:08:18 +0100

    Update practiceTwo.js

--- practiceTwo.js
+++ practiceTwo.js
@@ -26,3 +26,73 @@ function mySum(...args){
     }
     return sum;
   }
+
+---------------------------------------------------------------------
+  // Javascript function call method - Example
+let name = {
+  fullName : function(){
+    return this.firstName + this.secondName;
+  };
+}
+
+let personOne = {
+  firstName : "Ifeoluwa",
+  secondName : "Ajiboye"
+}
+let personTwo = {
+  firstName : "Ajibola",
+  secondName : "Ajiboye"
+}
+
+console.log(name.fullName.call(personOne));
+
+---------------------------------------------------------------------
+// Bind method - an object can borrow a method from another object.
+// apply method - make use of array;
+---------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------------
+/* Define a recursive function isEven corresponding to this description. 
+The function should accept a single parameter (a positive, whole number) and return a Boolean. */
+function isEven(n){
+  n = Math.abs(n);
+  if (n === 0){
+    return true;
+  }
+  else if (n === 1){
+    return false;
+  }
+  else{
+    return isEven(n - 2);
+  }
+}
+
+
+--------------------------------------------------------------------
+/* Write a function countBs that takes a string as its only argument and returns a number that indicates 
+how many uppercase B characters there are in the string. */
+function countB(str){
+  let count = 0;
+  for (let i = 0; i < str.length; i++){
+    if ( str[i] === "B"){
+      count++;
+    }
+  }
+  return count;
+}
+
+--------------------------------------------------------------------
+/* Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates 
+the character that is to be counted (rather than counting only uppercase B characters). Rewrite countBs to make use of this new function */
+ function countChar(str, char){
+   let count = 0;
+   for (let i = 0; i < str.length; i++){
+     if (str[i] === char){
+       count++
+     }
+  }
+   return count;
+ }
+--------------------------------------------------------------------
+
+--------------------------------------------------------------------
