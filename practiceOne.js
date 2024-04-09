99f7b7a532e912090ac63fa1e88f8623f8a1a1da
Ifeoluwa-Ajiboye <162115703+Ifeoluwa-Ajiboye@users.noreply.github.com>
2024-04-09 17:58:54 +0100

    Update practiceOne.js

--- practiceOne.js
+++ practiceOne.js
@@ -6,7 +6,6 @@ do {
   console.log(yourName);
 }
 while(!yourName);
-
 ------------------------------------------------------------------------------------------
 //power of
 let number = 0;
@@ -42,6 +41,10 @@ else{
   console.log("#".repeat(i));
 }
 
+      ***********
+        
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
 ------------------------------------------------------------------------------------------
 /* Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
 For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. */
@@ -59,7 +62,14 @@ for(let i = 0; i < 100; i++){
   }
   console.log(output);
 }
+    ************
 
+for (let n = 0; n < 100; n++){
+  let output = "";
+  if (n % 2 === 0) output += "fizz";
+  if (n % 5 === 0 && n % 3 !== 0) output += "buzz";
+  console.log(output || n);
+    }
 ------------------------------------------------------------------------------------------
 /* Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
 For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. */
@@ -77,6 +87,14 @@ for (let i = 0; i < 100; i++){
   }
   console.log(output)
 }
+
+    ***************
+for (let n = 0; n < 100; n++){
+  let output = "";
+  if (n % 2 === 0) output += "fizz";
+  if (n % 5 === 0) output += "buzz";
+  console.log(output || n);
+    }
 ------------------------------------------------------------------------------------------
 /* Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
 At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
